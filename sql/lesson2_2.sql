19b9bf423bc1fb2d5ef0164760a86a426da0f763
MAUROTU-TW <tumauro@icloud.com>
2024-09-29 16:08:57 +0800

    lesson2_3

--- sql/lesson2_2.sql
+++ sql/lesson2_2.sql
@@ -1,11 +1,22 @@
 DROP TABLE if exists hospital;
 
 create table if not exists hospital(
-	序號 serial primary key,
-	直轄市或省轄縣市 varchar(5) not null,
-	醫療機構名稱 varchar(30) not null unique,
-	市話 varchar(15),
-	分機 varchar(7),
-	聯絡人 varchar(7),
-	地址 varchar(50) 
+	id serial primary key,
+	state varchar(5) not null,
+	name varchar(30) not null unique,
+	phone varchar(15),
+	ext varchar(7),
+	contact varchar(7),
+	address varchar(50) 
     );
+
+
+create table if not exists weather(
+	id serial primary key,
+	city varchar(20)not null,
+	startdate timestamp,
+	enddate timestamp,
+	hight real,
+	low real,
+	status varchar(7)
+	);
