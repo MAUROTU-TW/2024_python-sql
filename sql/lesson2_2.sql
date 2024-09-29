918e67e4c7488073f74771d08512a87f42f779f7
MAUROTU-TW <tumauro@icloud.com>
2024-09-29 14:38:13 +0800

    lesson2_2

---
+++ sql/lesson2_2.sql
@@ -0,0 +1,11 @@
+DROP TABLE if exists hospital;
+
+create table if not exists hospital(
+	序號 serial primary key,
+	直轄市或一般縣市 varchar(5) not null,
+	醫療機構名稱 varchar(30) not null unique,
+	市話 varchar(15),
+	分機 varchar(7),
+	聯絡人 varchar(7),
+	地址 varchar(50) 
+    );
