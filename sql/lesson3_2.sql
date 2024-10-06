bcfb32c2c01849ed9c1552f2c074a2f2e6712aae
MAUROTU-TW <tumauro@icloud.com>
2024-10-06 11:53:13 +0800

    modify

--- sql/lesson3_2.sql
+++ sql/lesson3_2.sql
@@ -1,4 +1,4 @@
-SELECT code,name
+SELECT (code,name)
 FROM station;
 
 SELECT *
@@ -8,4 +8,5 @@ SELECT code as 代碼
 FROM station;
 
 SELECT code as 代碼, name as 車站名稱, e_name as 英文名稱
-FROM station;
+FROM station
+WHERE code='1001'
