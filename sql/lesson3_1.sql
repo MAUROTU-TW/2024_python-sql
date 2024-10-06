8ec896c7ae44483c71bc80648c69a2be871b3d9e
MAUROTU-TW <tumauro@icloud.com>
2024-10-06 10:55:31 +0800

    modify

---
+++ sql/lesson3_1.sql
@@ -0,0 +1,39 @@
+DROP TABLE if exists station;
+
+create table if not exists hospital(
+	id serial primary key,
+	state varchar(5) not null,
+	name varchar(30) not null unique,
+	phone varchar(15),
+	ext varchar(7),
+	contact varchar(7),
+	address varchar(50) 
+    );
+
+
+create table if not exists weather(
+	id serial primary key,
+	city varchar(20)not null,
+	startdate timestamp,
+	enddate timestamp,
+	hight real,
+	low real,
+	status varchar(7)
+	);
+
+
+create table if not exists station(
+	id serial primary key,
+	code varchar(20)not null unique,
+	name varchar(50)unique,
+	e_name varchar(50)
+	);
+
+ALTER TABLE stations
+ALTER COLUMN name 
+TYPE VARCHAR(15);
+
+ALTER TABLE stations
+DROP CONSTRAINT e_name;
+
+DROP TABLE IF EXISTS stations;	
