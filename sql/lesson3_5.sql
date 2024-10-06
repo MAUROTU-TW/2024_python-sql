6a24f88a9d73a50b45cb9a266f5d433b8490d3ae
MAUROTU-TW <tumauro@icloud.com>
2024-10-06 16:12:30 +0800

    MODIFY

--- sql/lesson3_5.sql
+++ sql/lesson3_5.sql
@@ -11,6 +11,12 @@ INSERT INTO student VALUES(2,'小洪','華語','50');
 INSERT INTO student VALUES(3,'小黃','日語','50');
 INSERT INTO student VALUES(4,'小黑','美語','50');
 
+INSERT INTO student 
+VALUES(1, '小白','英語',50),
+      (2, '小洪','華語',50),
+      (3, '小黃','日語',50),
+      (4, '小黑','美語',50)
+RETURNING *;
 
 DELETE FROM student; /*全刪*/
 
@@ -20,6 +26,10 @@ WHERE name ='小白';
 DROP TABLE IF EXISTS student;
 
 SELECT *
-FROM student;
+FROM student
 
+UPDATE student
+SET major = '英語文學'
+WHERE major = '英語'
+RETURNING *;
 
