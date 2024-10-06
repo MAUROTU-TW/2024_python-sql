c94278050f8bc3780d863ab9b0a8d1a2d26949b3
MAUROTU-TW <tumauro@icloud.com>
2024-10-06 15:02:47 +0800

    modify

--- sql/lesson3_4.sql
+++ sql/lesson3_4.sql
@@ -4,3 +4,26 @@ create TABLE student(
 	major VARCHAR(20),
 	PRIMARY KEY(student_id)
 );
+
+SELECT *
+FROM student;
+
+/*新增資料*/
+INSERT INTO student(name,major)
+VALUES('小白','歷史');
+
+INSERT INTO student(major,name)
+VALUES('生物','小黑');
+
+INSERT INTO student(major,name)
+VALUES(NULL,'小李');
+
+/*新增多筆*/
+INSERT INTO student(major,name)
+VALUES('華語','小李'),(數學,'小李')
+
+
+/*傳回新增資訊*/
+INSERT INTO student(major,name)
+VALUES('華語','小李'),('數學','小李')
+RETURNING *;
