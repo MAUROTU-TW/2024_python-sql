59c181a2e4ee80112333054054d23ea4c020495f
MAUROTU-TW <tumauro@icloud.com>
2024-10-13 16:10:21 +0800

    MODIFY

--- sql/lesson4_4_1.sql
+++ sql/lesson4_4_1.sql
@@ -23,3 +23,42 @@ GROUP BY 日期
 ORDER BY 日期 ASC;
 
 
+SELECT customer_id,SUM(amount)AS 合計
+FROM payment
+GROUP BY customer_id
+HAVING SUM(amount)>200;
+
+
+SELECT store_id,COUNT(customer_id)AS 客戶數
+FROM customer
+GROUP BY store_id
+HAVING COUNT(customer_id)>300;
+
+/*subquery*/
+SELECT *
+FROM country;
+
+SELECT country_id
+FROM country
+WHERE country='Taiwan';
+
+SELECT city
+FROM city
+WHERE country_id=92;
+
+
+SELECT city
+FROM city
+WHERE country_id=(
+	SELECT country_id
+	FROM country
+	WHERE country='Taiwan'
+);
+
+
+
+
+
+
+
+
