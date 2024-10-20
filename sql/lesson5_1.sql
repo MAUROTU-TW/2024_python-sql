5f39d50f200737173ce6b937c1cde0bdfe0b69a7
MAUROTU-TW <tumauro@icloud.com>
2024-10-20 13:35:41 +0800

    MODIFY

--- sql/lesson5_1.sql
+++ sql/lesson5_1.sql
@@ -43,4 +43,40 @@ SELECT stationname AS 站名, TO_CHAR(date,'YYYY-MM') AS 日期,SUM(gateincoming
 FROM stations JOIN station_in_out ON stationcode = stacode
 /*WHERE date BETWEEN '2022-01-01' AND '2022-12-31';*/
 WHERE EXTRACT(YEAR FROM date) = 2022 AND stationname='基隆'
-GROUP BY 站名,日期;
+GROUP BY 站名,日期
+ORDER BY 進站人數小計 DESC;
+
+/*基隆火車站20202021 2022,每年份進站人數*/
+SELECT stationname AS 站名, EXTRACT(YEAR FROM date) AS 年分,SUM(gateincomingcnt) AS 當年總進站人數
+FROM stations JOIN station_in_out ON stationcode = stacode
+WHERE EXTRACT(YEAR FROM date) IN (2020,2021,2022) AND stationname='基隆'
+group BY 站名,年分;
+
+SELECT stationname AS 站名, EXTRACT(YEAR FROM date) as 年份,SUM(gateincomingcnt) AS 當年總進站人數
+FROM stations JOIN station_in_out ON stationcode = stacode
+WHERE EXTRACT(YEAR FROM date) IN (2020,2021,2022) AND stationname in('基隆','臺北')
+GROUP BY 站名,年份;
+
+/*進站人數最多的一筆*/
+SELECT stationname AS 站名,gateincomingcnt AS 進站人數
+FROM stations JOIN station_in_out ON stationcode = stacode
+WHERE gateincomingcnt = (
+	SELECT MAX(gateincomingcnt)
+	FROM station_in_out
+)
+
+/*各站點進站人數最多的一筆*/
+SELECT stationname AS 站名,gateincomingcnt AS 進站人數,date
+FROM stations JOIN station_in_out ON stationcode = stacode
+WHERE (stationname,gateincomingcnt) in (
+	SELECT stationname AS 站名,MAX(gateincomingcnt)AS最多人數
+	FROM stations JOIN station_in_out ON stationcode = stacode
+	Group by 站名
+)
+ORDER BY gateincomingcnt DESC;
+
+
+
+SELECT stationname AS 站名,MAX(gateincomingcnt)AS最多人數
+FROM stations JOIN station_in_out ON stationcode = stacode
+Group by 站名
