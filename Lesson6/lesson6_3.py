3e39866f27d7589051105e1a581cbb181bfdc41f
MAUROTU-TW <tumauro@icloud.com>
2024-10-27 15:23:58 +0800

    modify

--- Lesson6/lesson6_3.py
+++ Lesson6/lesson6_3.py
@@ -7,9 +7,18 @@ import psycopg2
 connection = psycopg2.connect(host='dpg-csehq5jtq21c738c82g0-a.singapore-postgres.render.com',database='render_a21',user='render_a21_user',password='LtyqwUeXcgYPiPe9v4oBjlzm0vyOV9l9')
 cursor = connection.cursor()
 cursor.execute("SELECT * FROM stations;")
-rows = cursor.fetchone()
+rows = cursor.fetchall()
 names = []
 for row in rows:
     names.append(row[2])
+
+option = st.selectbox(
+    "請選擇您最愛的車站?",
+    names
+)
+
+st.write("您的最愛是:", option)
+
     
-st.write(names)
+#st.write(names)
+
