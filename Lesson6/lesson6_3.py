917be595450ff2beed5b3a0c9af411f3922bef37
MAUROTU-TW <tumauro@icloud.com>
2024-10-27 14:41:21 +0800

    modify

--- Lesson6/lesson6_3.py
+++ Lesson6/lesson6_3.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
-st.write("# Hello!sreamlitWord")
-#st.write_stream("Hello!sreamlitWord")
+#st.write("# Hello!sreamlitWord")
+
+
+import psycopg2
+connection = psycopg2.connect(host='dpg-csehq5jtq21c738c82g0-a.singapore-postgres.render.com',database='render_a21',user='render_a21_user',password='LtyqwUeXcgYPiPe9v4oBjlzm0vyOV9l9')
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM stations;")
+rows = cursor.fetchone()
+names = []
+for row in rows:
+    names.append(row[2])
+    
+st.write(names)
