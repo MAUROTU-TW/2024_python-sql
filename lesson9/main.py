49e21f128d5659b86c8b507cd0305cfacb743a55
MAUROTU-TW <tumauro@icloud.com>
2024-12-29 21:22:48 +0800

    modify

--- lesson9/main.py
+++ lesson9/main.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import streamlit as st
 from time import sleep
-from datasource import get_data
+#from datasource import get_data
+from tools import get_data
 
 load_dotenv()
 
